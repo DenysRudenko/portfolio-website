6e1f86ed15afaba1b52d0cd49e40b276bbfbd3b9
Denys Rudenko <69275031+DenysRudenko@users.noreply.github.com>
2024-08-03 16:38:27 +0100

    fix(): edited menu section, and fixed the bug with freezing, added js cript for animation

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,11 @@ const hamburger = document.querySelector('.hamburger'),
 
 hamburger.addEventListener('click', () => {
 	menu.classList.add('active');
+	menu.style.transition = 'all .4s ease';
 });
 
 closeElem.addEventListener('click', () => {
 	menu.classList.remove('active');
-})
+	menu.style.transition = 'all .4s ease';
+});
 
